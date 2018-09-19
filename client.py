4372317e8c192e7f7d20be8094634e9528851930
bingo12128 <21631133@zju.edu.cn>
2018-09-19 09:14:31 +0800

    client-server

---
+++ client.py
@@ -0,0 +1,11 @@
+from suds.client import Client
+hello_client = Client('http://localhost:7789/?wsdl')
+hello_client.options.cache.clear()
+rules={}
+rules["username"]="alle"
+rules["emotion"]="1-2-3"
+
+print rules
+result = hello_client.service.get_recommend(rules)
+print result
+
